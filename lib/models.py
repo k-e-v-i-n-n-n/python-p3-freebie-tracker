463744cd848ac4c6463834f468f0ca786c7f40c2
Kevin Breid <kevinbreid@yahoo.com>
2024-03-21 16:36:54 +0100

    Writing association tables and fine tuning relationships

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Float, Table
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,6 +9,12 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+dev_companies = Table('dev_companies', Base.metadata,
+    Column('company_id', Integer, ForeignKey('companies.id')),
+    Column('dev_id', Integer, ForeignKey('devs.id')),
+    extend_existing=True
+)
+
 class Company(Base):
     __tablename__ = 'companies'
 
@@ -16,8 +22,9 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
-    devs = relationship('Dev', backref=backref('company'))
-    freebies = relationship('Freebie', secondary='freebies', back_populates='company')
+    devs = relationship('Dev', secondary=dev_companies, back_populates='companies')
+    freebies = relationship('Freebie', backref=backref('companies'))
+    
 
     def __repr__(self):
         return f'<Company {self.name}>'
@@ -27,7 +34,7 @@ class Freebie(Base):
 
     id = Column(Integer(), primary_key=True)
     item_name = Column(String())
-    value = Column(Integer())
+    value = Column(Float())
 
     company_id = Column(Integer(), ForeignKey('companies.id'))
     dev_id = Column(Integer(), ForeignKey('devs.id'))
@@ -38,8 +45,9 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
-    companies = relationship('Company', backref=backref('dev'))
-    freebies = relationship('Freebie', secondary='freebies', back_populates='dev')
+    companies = relationship('Company', secondary=dev_companies, back_populates='devs')
+    freebies = relationship('Freebie', backref=backref('devs'))
+
 
     def __repr__(self):
         return f'<Dev {self.name}>'
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,76 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+
+
+from faker import Faker
+import random
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Dev, Company, Freebie
+
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///many_to_many.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(Dev).delete()
+    session.query(Company).delete()
+    session.query(Freebie).delete()
+
+    fake = Faker()
+
+
+    devs = []
+
+    for i in range(50):
+        dev = Dev(name=fake.unique.name())
+
+        session.add(dev)
+        session.commit()
+        devs.append(dev)
+
+    companies = []
+    
+    for i in range(50):
+        company = Company(
+            name = fake.unique.name(),
+            founding_year = random.randint(1900, fake.year())
+            )
+        session.add(company)
+        session.commit()
+        companies.append(company)
+        
+    freebies = []
+
+    for company in companies:
+        for i in random.randint(1,6):
+            dev = random.choice(devs)
+            if company not in dev.companies:
+                dev.companies.append(company)
+                session.add(dev)
+                session.commit()
+            
+            freebie = Freebie(
+                
+                item_name = fake.name(),
+                value = fake.currency(),
+
+                dev_id = dev.id,
+                company_id = company.id
+            )
+
+            freebies.append(freebie)
+
+
+    session.bulk_save_objects(freebies)
+    session.commit()
+
+
+
+    # session.bulk_save_objects(reviews)
+    # session.commit()
+    # session.close()
